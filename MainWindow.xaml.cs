369ded82cd5d9adcdcb3cfe9c053eeb54194a7ed
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2019-01-25 02:54:04 +0900

    [UPDATE] Add button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TimeManager
         {
             InitializeComponent();
         }
+
+        private void BtnClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
